da0895cbcb6ae3deedd4f48a69d22812383ca548
GorbunovDanil <danil.gorbunov@tul.cz>
2024-11-08 17:57:52 +0100

    Test commit

--- src/components/home/components/About.jsx
+++ src/components/home/components/About.jsx
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
+import Accordion from "react-bootstrap/Accordion";
 
 import answer from "../../../assets/steps/answer.png";
 import dominikZlebek from "../../../assets/photos/dominik-zlebek.png";
 import janMusil from "../../../assets/photos/jan-musil.png";
+import plus from "../../../assets/plus.png";
 
 export default function About() {
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+    setSelected(i);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center lg:mx-[100px] xl:py-[100px] xl:px-[200px] mx-14 gap-12 mb-40 xl:mb-0">
       <h2 className="font-inter text-white font-semibold text-3xl align-center">
@@ -18,15 +29,44 @@ export default function About() {
             className="rounded-md w-[300px] lg:w-full object-cover"
           />
           <div className="flex flex-col gap-5">
-            <div className="flex flex-col gap-1">
-              <div className="text-[#C6BFFF] text-xl lg:text-2xl">
-                Ing. Dominik Žlebek, LL.M.
+            <div className="flex flex-col items-center justify-between gap-10">
+              <div className="flex flex-col gap-1">
+                <div className="text-[#C6BFFF] text-xl lg:text-2xl">
+                  Ing. Dominik Žlebek, LL.M.
+                </div>
+                <div className="text-white text-lg lg:text-lg">
+                  specialista veřejných zakázek
+                </div>
               </div>
-              <div className="text-white text-lg lg:text-lg">
-                specialista veřejných zakázek
+              <div>
+                <img
+                  src={plus}
+                  alt=""
+                  className="w-[30px] h-[30px] invert"
+                  onClick={() => toggle(0)}
+                />
               </div>
             </div>
-            <div className="text-white lg:text-lg">
+            <Accordion defaultActiveKey="0">
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>More info</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+            <div
+              className={`description text-white lg:text-lg ${
+                selected === 0 ? "show" : ""
+              }`}
+            >
               Dominik Žlebek je absolventem Provozně ekonomické fakulty na České
               zemědělské univerzitě v Praze, kde vystudoval obor Podnikání a
               administrativa a získal akademický titul Ing. Již během studia se
@@ -49,7 +89,7 @@ export default function About() {
             className="rounded-md w-[300px] lg:w-full object-cover"
           />
           <div className="flex flex-col gap-5">
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-1" onClick={() => toggle(1)}>
               <div className="text-[#C6BFFF] text-xl lg:text-2xl">
                 Ing. Dominik Žlebek, LL.M.
               </div>
@@ -57,7 +97,11 @@ export default function About() {
                 specialista veřejných zakázek
               </div>
             </div>
-            <div className="text-white lg:text-lg">
+            <div
+              className={`text-white lg:text-lg ${
+                selected === 1 ? "show" : ""
+              }`}
+            >
               Dominik Žlebek je absolventem Provozně ekonomické fakulty na České
               zemědělské univerzitě v Praze, kde vystudoval obor Podnikání a
               administrativa a získal akademický titul Ing. Již během studia se
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
 import logo from "../../assets/logo/logo.png";
+import menu from "../../assets/menu.png";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Function to toggle menu state
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="hidden glass-box-nav bg-black lg:h-[120px] xl:h-[90px] p-[20px] lg:px-[40px] px-[50px] lg:grid lg:grid-cols-3">
+    <nav className="relative glass-box-nav bg-black h-[90px] lg:h-[120px] xl:h-[90px] lg:px-[40px] p-[20px] flex justify-between lg:grid lg:grid-cols-3">
       <div className="flex gap-2 justify-start items-center">
         <img src={logo} alt="Logo" className="w-[50px] h-[50px]" />
         <div>
           <div className=" text-white font-bold">TENDERIX</div>
-          <div className=" text-white lg:hidden xl:block">aktuální zakázková praxe</div>
+          <div className=" text-white hidden xl:block">
+            aktuální zakázková praxe
+          </div>
         </div>
       </div>
-      <div className="flex gap-4 justify-center items-center">
+      <div className="lg:flex gap-4 justify-center items-center hidden">
         <div className="glass-box-menu text-white text-lg bg-[#444444] px-3 py-1 rounded-lg">
           O nás
         </div>
@@ -20,9 +30,26 @@ export default function Header() {
         <div className="text-white text-lg">Kontakt</div>
       </div>
       <div className="flex gap-4 justify-end items-center">
-        <div className="glass-box-login text-white bg-[#303030] px-3 py-1 rounded-lg text-lg border-2 border-[#949494]">Přihlásit</div>
-        <div className="glass-box-register text-black bg-[#ffffff] px-3 py-1 rounded-lg text-lg border-2 border-[#767676]">Vyzkoušet zdarma</div>
+        <div className="hidden lg:block glass-box-login text-white bg-[#303030] px-3 py-1 rounded-lg text-lg border-2 border-[#949494]">
+          Přihlásit
+        </div>
+        <div className="glass-box-register text-black bg-[#ffffff] px-3 py-1 rounded-lg text-lg border-2 border-[#767676]">
+          Vyzkoušet zdarma
+        </div>
       </div>
+
+      <div
+        className="absolute right-[20px] -bottom-[75px] rounded-full glass-box-menu-icon p-4 z-50 cursor-pointer"
+        onClick={toggleMenu}
+      >
+        <img src={menu} alt="Menu" className="w-[25px] h-[25px] invert" />
+      </div>
+
+      <div
+        className={`bg-green-400 h-screen w-screen absolute top-0 left-0 transform duration-300 z-40 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      ></div>
     </nav>
   );
 }
